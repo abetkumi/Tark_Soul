0a0fd14bcf22b6e8355bc435dccfe848491d4f16
abetkumi <ca01234001@st.kawahara.ac.jp>
2025-04-30 11:47:55 +0900

    エネミーの追跡、攻撃の移行実装

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -21,14 +21,21 @@ public class EnemyScript : MonoBehaviour
 
     //Enemyステータス用変数
     EnemyStatus m_enemyStatus = EnemyStatus.Idle;
+    
     //アニメーション用変数
     private Animator m_animator;
     private NavMeshAgent m_agent;
+
+    //アタック用変数
+    [SerializeField] SphereCollider m_attackCollider;
+    EnemyAttackScript m_enemyAttackScript;
+    public float m_rotationSpeed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         m_playerScript = m_playerObject.GetComponent<PlayerScript>();
+        m_enemyAttackScript = GetComponent<EnemyAttackScript>();
         m_animator = GetComponent<Animator>();
         m_agent = GetComponent<NavMeshAgent>();
     }
@@ -36,11 +43,13 @@ public class EnemyScript : MonoBehaviour
     private void doInit()
     {
         m_enemyStatus = EnemyStatus.Idle;
+        m_agent.SetDestination(transform.position);
     }
 
     private void doSearch()
     {
-        if (Vector3.Distance(transform.position, m_playerPosition) < 5.0f)
+        //プレイヤーが近づくと追跡ステートに移行する
+        if (Vector3.Distance(transform.position, m_playerPosition) <= 10.0f)
         {
             m_enemyStatus = EnemyStatus.Walk;
             m_animator.SetBool("IdleFlag", false);
@@ -48,22 +57,78 @@ public class EnemyScript : MonoBehaviour
     }
     private void doMove()
     {
+        //ナビメッシュのターゲットをプレイヤーの位置に変更する
         m_agent.SetDestination(m_playerPosition);
+
+        //攻撃ステートに移行する
+        if(Vector3.Distance(transform.position,m_playerPosition) <= 3.0f)
+        {
+            m_enemyStatus = EnemyStatus.Attack;
+            m_agent.SetDestination(transform.position);
+            m_animator.SetBool("WalkFlag", false);
+        }
+
+        //待機ステートに移行する
+        if (Vector3.Distance(transform.position, m_playerPosition) > 10.0f)
+        {
+            m_enemyStatus = EnemyStatus.Idle;
+            m_agent.SetDestination(transform.position);
+            m_animator.SetBool("WalkFlag", false);
+        }
     }
 
     private void doAttack()
     {
+        ////攻撃中プレイヤーの方向を向く
+        //Vector3 direction = m_playerPosition - transform.position;  // プレイヤー方向のベクトル
+        //direction.y = 0;
+        //Quaternion m_targetRotation = Quaternion.LookRotation(direction);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, m_targetRotation, m_rotationSpeed * Time.deltaTime);
+        
+        //攻撃判定を有効にする
+        m_attackCollider.enabled = true;
+    }
 
+  
+
+    private void AttackEnd()
+    {
+        //プレイヤーが攻撃範囲内にいると再び攻撃
+        if (Vector3.Distance(transform.position, m_playerPosition) <= 3.0f)
+        {
+            // エネミーの正面をプレイヤーに向ける（補完させたい）
+            Vector3 direction = m_playerPosition - transform.position;  // プレイヤー方向のベクトル
+            direction.y = 0;  // Y軸方向の回転を無効にして、XZ平面のみで回転させる
+            transform.rotation = Quaternion.LookRotation(direction);  // プレイヤーの方向を向く
+        }
+        //プレイヤーが近くにいると追跡状態
+        else if (Vector3.Distance(transform.position, m_playerPosition) <= 10.0f)
+        {
+            //ステータスを歩くに変更
+            m_enemyStatus = EnemyStatus.Walk;
+            m_animator.SetBool("AttackFlag", false);
+            //攻撃判定を無効にする
+            m_attackCollider.enabled = false;
+            Debug.Log("攻撃終了");
+        }
+        //プレイヤーが離れていると待機状態
+        else
+        {
+            //ステータスを待機状態に変更
+            m_enemyStatus = EnemyStatus.Idle;
+            m_animator.SetBool("AttackFlag", false);
+            Debug.Log("攻撃終了");
+        }
     }
 
     private void doDamage()
     {
-
+        Debug.Log("敵がダメージを受けた");
     }
 
     private void doDeath()
     {
-
+        Debug.Log("敵が4んだ");
     }
 
     private void doAnimation()
@@ -80,9 +145,11 @@ public class EnemyScript : MonoBehaviour
                 break;
             case EnemyStatus.Attack:
                 m_animator.SetBool("AttackFlag", true);
+                doAttack();
                 break;
             case EnemyStatus.Damage:
                 m_animator.SetBool("DamageFlag", true);
+                doDamage();
                 break;
             case EnemyStatus.Death:
                 m_animator.SetBool("DeathFlag", true);
@@ -90,6 +157,7 @@ public class EnemyScript : MonoBehaviour
                 m_animator.SetBool("WalkFlag", false);
                 m_animator.SetBool("AttackFlag", false);
                 m_animator.SetBool("DamageFlag", false);
+                doDeath();
                 break;
             default:
                 break;
