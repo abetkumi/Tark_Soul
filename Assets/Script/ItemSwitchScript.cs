8320cfba170db8600f2e439285b981c5f3ad5605
abetkumi <ca01234001@st.kawahara.ac.jp>
2025-05-02 11:05:18 +0900

    ゲートアニメーション（仕組みは完成）

---
+++ Assets/Script/ItemSwitchScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSwitchScript : MonoBehaviour
+{
+    [SerializeField] GameObject Gate_right;
+    [SerializeField] GameObject Gate_left;
+
+    Animator m_animator_Lever;
+    Animator m_animator_Right;
+    Animator m_animator_Left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator_Lever = GetComponent<Animator>();
+        m_animator_Right = Gate_right.GetComponent<Animator>();
+        m_animator_Left = Gate_left.GetComponent<Animator>();
+    }
+
+    public void OnTriggerStay(Collider col)
+    {
+        if(col.tag=="Player")
+        {
+            if (Input.GetButton("Action"))
+            {
+                m_animator_Lever.SetTrigger("Gate Open");
+                m_animator_Right.SetTrigger("Gate Open");
+                m_animator_Left.SetTrigger("Gate Open");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
