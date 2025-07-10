c8a65dbe8b88fdcf31bbf9bb04478d4a4163c55d
gimute <y4k2t36s2416@gmail.com>
2025-07-10 14:22:20 +0900

    GameOver機能追加

--- Assets/Script/Player/PlayerStateMachine/PlayerStateDead.cs
+++ Assets/Script/Player/PlayerStateMachine/PlayerStateDead.cs
@@ -1,12 +1,15 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStateDead : IPlayerStateScript
 {
     private GameObject _player; //プレイヤー
     private PlayerScript _playerScript; //プレイヤーのスクリプト
     private Animator _animator;
+    private GameObject _GameOverUI;
 
     public PlayerStateDead(GameObject insertPlayer)
     {
@@ -18,5 +21,27 @@ public class PlayerStateDead : IPlayerStateScript
     public override void Start()
     {
         _animator.CrossFadeInFixedTime("Death", 0.3f);
+        _GameOverUI = UIManager.GetUIManager().NewUI(4);
+        _GameOverUI.GetComponent<GameOverUI>().FadeIn();
+
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void AnimationEvent(string EventName)
+    {
+        if (EventName == "AnimationEnd")
+        {
+            GameOver();
+        }
+    }
+
+    async void GameOver()
+    {
+        //タイトルシーンに移動する
+        await SceneManager.LoadSceneAsync("Title").ToUniTask();
     }
 }
